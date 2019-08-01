f9739f55dc36af4a28bd2b3c610885df52d88638
wfan19 <1472451850@qq.com>
2019-08-01 11:18:00 +0800

    added file selection

--- app/src/main/java/com/example/charades/CsvReader.java
+++ app/src/main/java/com/example/charades/CsvReader.java
@@ -1,26 +1,42 @@
 package com.example.charades;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.net.Uri;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CsvReader {
 
     private Context context;
     private String path;
+    private Uri uri;
     private ArrayList<String[]> rows = new ArrayList<>();
+
+    private final static String TAG = "CsvReader";
+
     public CsvReader(Context ctx, String p)
     {
         this.context = ctx;
         path = p;
     }
 
-    public void read() throws IOException
+    public CsvReader(Context ctx, Uri u)
+    {
+        this.context = ctx;
+        uri = u;
+    }
+
+    public void readFromAsset() throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(context.getAssets().open(path)));
         String line;
@@ -29,11 +45,34 @@ public class CsvReader {
         while ((line = br.readLine()) != null) {
             String[] row = line.split(separator);
             rows.add(row);
+            Log.d(TAG, "New row: " + Arrays.toString(row));
         }
+    }
+
+    public void readFromFile() throws IOException
+    {
+        ContentResolver cr = context.getContentResolver();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(cr.openInputStream(uri)));
+            String line;
+            String separator = ",";
+
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(separator);
+                rows.add(row);
+                Log.d(TAG, "New row: " + Arrays.toString(row));
+            }
+        } catch(FileNotFoundException e){e.printStackTrace();}
+
     }
 
     public ArrayList<String[]> getRows()
     {
         return rows;
+    }
+
+    public void clear()
+    {
+        rows.clear();
     }
 }
--- app/src/main/java/com/example/charades/MainActivity.java
+++ app/src/main/java/com/example/charades/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.charades;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,12 +33,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Import();
+        hideUI();
+
+        ImportFromAsset();
 
         tv_word = findViewById(R.id.tv_word);
         btn_next = findViewById(R.id.btn_next);
         tv_word.setText("" + words.get(0));
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus)
+            hideUI();
+    }
+
+    private void hideUI()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     int remaining;
@@ -52,18 +83,18 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Word> words = new ArrayList<>();
     private ArrayList<Word> done = new ArrayList<>();
 
-    private void Import()
+    private void ImportFromAsset()
     {
         try {
 
             CsvReader reader = new CsvReader(this,"words.csv");
-            reader.read();
-                for (String[] row: reader.getRows()) {
-                    Word word = new Word(row[0], row[1]);
-                    Log.d(TAG, "New word: " + word);
-                    bank.add(word);
-                    words.add(word);
-                }
+            reader.readFromAsset();
+            for (String[] row: reader.getRows()) {
+                Word word = new Word(row[0], row[1]);
+                Log.d(TAG, "New word: " + word);
+                bank.add(word);
+                words.add(word);
+            }
         }catch(IOException e){e.printStackTrace();}
         Log.d(TAG, "Shuffling words");
         Collections.shuffle(words);
@@ -81,5 +112,66 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "Word: " + word);
 
         done.clear();
+        finished = done.size() + 1;
+        remaining = words.size() - 1;
+        Log.d(TAG, "Finished words: " + finished);
+        Log.d(TAG, "Words remaining: " + remaining);
     }
+
+    private void clear()
+    {
+        Log.d(TAG, "Clearing words");
+        done.clear();
+        words.clear();
+        bank.clear();
+    }
+
+    public final int FILE_REQUEST_CODE = 12345;
+    public void Import(View view)
+    {
+        Log.d(TAG, "Import()...");
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("*/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        //String[] mimeTypes = {"file/csv"};
+        //intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent,FILE_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case FILE_REQUEST_CODE:
+                    Log.d(TAG, "onActivityResult...");
+                    clear();
+                    Uri selectedFile = data.getData();
+                    Log.d(TAG, "Path? " + selectedFile.getPath());
+                    CsvReader reader = new CsvReader(this, selectedFile);
+                    try {
+                        reader.readFromFile();
+                        for (String[] row: reader.getRows()) {
+                            Word word = new Word(row[0], row[1]);
+                            Log.d(TAG, "New word: " + word);
+                            bank.add(word);
+                            words.add(word);
+                        }
+                    }catch(IOException e){e.printStackTrace();}
+                    Log.d(TAG, "Shuffling words");
+                    Collections.shuffle(words);
+                    for(Word word : words)
+                        Log.d(TAG, "Word: " + word);
+
+                    tv_word.setText("" + words.get(0));
+                    remaining = words.size() - 1;
+                    finished  = done.size() + 1;
+                    Log.d(TAG, "Finished words: " + finished);
+                    Log.d(TAG, "Words remaining: " + remaining);
+
+                    break;
+            }
+    }
+
 }
